92d44437464777cc1f9ddceeaee7c5b8dc023932
Julia Prestes <71856252+juliaprestes@users.noreply.github.com>
2021-10-26 21:03:41 -0300

    Add files via upload

---
+++ ByteBank/Funcionarios/Diretor.cs
@@ -0,0 +1,30 @@
+﻿using ByteBank.Sistemas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Diretor : FuncionarioAutenticavel
+        //diretor é também um funcionário (heranca)
+    {
+        public Diretor( string cpf) : base(5000, cpf)
+        {
+            Console.WriteLine("Criando DIRETOR");
+        }
+
+        
+
+        public override void AumentarSalario()
+        {
+            Salario *= 1.15;
+        }
+        public override double GetBonificacao()
+        {
+
+            return Salario  * 0.5;
+        }
+    }
+}
